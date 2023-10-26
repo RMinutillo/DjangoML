261f54aa22904118430df4a7ed0b782cb85417db
RMinutillo <ramirominutillo@gmail.com>
2023-10-26 13:48:36 -0300

    Mensaje de confirmación

---
+++ EasyDjangoProject/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+def portada(request):
+    return render(request,'portada.html')
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form,get_user())
+            return redirect('panel_de_control')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'inicio_sesion.html', {'form':form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('portada')
+
+@login_required
+def panel_de_control(request):
+    return render(request, 'panel_de_control.html')
+
+
