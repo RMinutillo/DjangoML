224628fb627d500c4bef515d94d50249bd18ff45
RMinutillo <ramirominutillo@gmail.com>
2023-12-15 20:34:03 -0300

    actualizacion funcionando

--- EasyDjangoProject/settings.py
+++ EasyDjangoProject/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    'C:/Users/ramir/OneDrive/Escritorio/Data science/DjangoML-main/EasyDjangoProject/templates/coffee-shop-html-template/static',
+    os.path.join(BASE_DIR, "static"),
 ]
 
 
